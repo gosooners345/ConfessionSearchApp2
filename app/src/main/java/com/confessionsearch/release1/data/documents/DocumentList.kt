8f895a1a3339dd41a230efa0daf5a58b73b09e84
gosooners345 <Boomersooner12345@gmail.com>
2021-11-01 20:58:03 -0400

    Documentation for DocumentList class

--- app/src/main/java/com/confessionsearch/release1/data/documents/DocumentList.kt
+++ app/src/main/java/com/confessionsearch/release1/data/documents/DocumentList.kt
@@ -2,6 +2,7 @@ package com.confessionsearch.release1.data.documents
 
 import java.util.*
 
+//DocumentList Class for Confessionsearchapp
 class DocumentList : ArrayList<Document>() {
     var title: String? = null
 
