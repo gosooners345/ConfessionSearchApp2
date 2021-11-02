117f13549f6c3974ee1c1f360ab1521154deb928
gosooners345 <Boomersooner12345@gmail.com>
2021-11-01 20:51:24 -0400

    Documentation for BibleContentsList Class

--- app/src/main/java/com/confessionsearch/release1/data/bible/BibleContentsList.kt
+++ app/src/main/java/com/confessionsearch/release1/data/bible/BibleContentsList.kt
@@ -1,5 +1,6 @@
 package com.confessionsearch.release1.data.bible
 
+//List Class for BibleContents Class for adding extra properties to class
 class BibleContentsList : ArrayList<BibleContents>() {
     var title: String? = null
 
