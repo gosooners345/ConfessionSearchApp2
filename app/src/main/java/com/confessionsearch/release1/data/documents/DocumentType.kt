e271f030d3420767889416b81630c679b1b23781
gosooners345 <Boomersooner12345@gmail.com>
2021-11-01 20:58:40 -0400

    Documentation for DocumentType Class

--- app/src/main/java/com/confessionsearch/release1/data/documents/DocumentType.kt
+++ app/src/main/java/com/confessionsearch/release1/data/documents/DocumentType.kt
@@ -1,5 +1,6 @@
 package com.confessionsearch.release1.data.documents
 
+//DocumentType Data Class for ConfessionSearchApp
 class DocumentType {
     var documentTypeID: Int? = null
     var documentTypeName: String? = null
