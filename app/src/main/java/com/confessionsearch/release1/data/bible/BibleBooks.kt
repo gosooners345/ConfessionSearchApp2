af1125412a5978b095cb833d2576bb02b52a2659
gosooners345 <Boomersooner12345@gmail.com>
2021-11-01 20:47:24 -0400

    Added Documentation for BibleBooks Class

--- app/src/main/java/com/confessionsearch/release1/data/bible/BibleBooks.kt
+++ app/src/main/java/com/confessionsearch/release1/data/bible/BibleBooks.kt
@@ -2,6 +2,7 @@ package com.confessionsearch.release1.data.bible
 
 import android.provider.BaseColumns
 
+//Bible Books Data Class for ConfessionSearchApp2
 class BibleBooks : BaseColumns {
     var BookID: Int? = null
     var BookName: String? = null
