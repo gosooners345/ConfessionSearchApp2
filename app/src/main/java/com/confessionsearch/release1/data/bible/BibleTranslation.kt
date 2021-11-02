26e1a6f04fcd5518e24858802b291ec23c75e35f
gosooners345 <Boomersooner12345@gmail.com>
2021-11-01 20:52:17 -0400

    Documentation for BibleTranslations Class

--- app/src/main/java/com/confessionsearch/release1/data/bible/BibleTranslation.kt
+++ app/src/main/java/com/confessionsearch/release1/data/bible/BibleTranslation.kt
@@ -2,6 +2,7 @@ package com.confessionsearch.release1.data.bible
 
 import android.provider.BaseColumns
 
+//Bible Translations Data Class for ConfessionSearchApp
 class BibleTranslation : BaseColumns {
     var bibleTranslationID: Int? = null
     var bibleTranslationName: String? = null
