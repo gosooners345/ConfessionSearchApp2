7aaca133a54c4471dde8d5072a627f52c87fa24a
Brandon James Guerin <boomersooner12345@gmail.com>
2021-12-01 12:45:45 -0500

    Reduce Number of Widgets on Search Results page to improve performance. Replaced help page with new cleaner About page class
    Signed-off-by: Brandon James Guerin <boomersooner12345@gmail.com>

--- app/src/main/java/com/confessionsearch/release1/ui/help/HelpPageFragment.kt
+++ app/src/main/java/com/confessionsearch/release1/ui/help/HelpPageFragment.kt
@@ -2,20 +2,17 @@ package com.confessionsearch.release1.ui.help
 
 import android.os.Bundle
 import android.text.Html
-import android.text.Html.*
-import android.util.Log
+import android.text.Html.FROM_HTML_MODE_COMPACT
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.confessionsearch.release1.MainActivity
 import com.confessionsearch.release1.R
-import mehdi.sakout.aboutpage.AboutPage
-import mehdi.sakout.aboutpage.Element
 
 class HelpPageFragment : Fragment() {
 
-    val newLine = "\r\n"
+    val newLine = "<br>"
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,7 +20,7 @@ class HelpPageFragment : Fragment() {
     ): View? {
 
 
-        var searchTitle = (getString(R.string.search_tab) + newLine +
+        /*var searchTitle = (getString(R.string.search_tab) + newLine +
                 getString(R.string.help_searchTab_pgh1))
 
         var notesTitle =
@@ -67,13 +64,29 @@ class HelpPageFragment : Fragment() {
 
 
 
-        return helpPage
-        /*val view = inflater.inflate(R.layout.fragment_help_page, container, false)
-      var searchTabTV = view.findViewById<TextView>(R.id.searchTabLabel)
-      var searchTabPgh1 = view.findViewById<TextView>(R.id.searchTabHelpText1)
+        return helpPage*/
+        val view = inflater.inflate(R.layout.fragment_help_page, container, false)
+        var searchTabTV = view.findViewById<TextView>(R.id.searchTabLabel)
+        searchTabTV.text = Html.fromHtml(
+            (getString(R.string.search_tab) + newLine + getString(R.string.help_searchTab_pgh1)),
+            FROM_HTML_MODE_COMPACT
+        )
+        var bibleTabTV = view.findViewById<TextView>(R.id.bibleTabLabel)
+        bibleTabTV.text = Html.fromHtml(
+            (getString(R.string.bible_tab_HelpLabel) + newLine + getString(R.string.bible_tab_helpPgh)),
+            FROM_HTML_MODE_COMPACT
+        )
+        var notesTabTV = view.findViewById<TextView>(R.id.notesTabLabel)
+        notesTabTV.text = Html.fromHtml(
+            (getString(R.string.notes_tab_helpLabel) + newLine + getString(R.string.notes_tab_helpPgh)),
+            FROM_HTML_MODE_COMPACT
+        )
+        var sourcesLabelTV = view.findViewById<TextView>(R.id.sourcesTabLabel)
+        sourcesLabelTV.text =
+            Html.fromHtml((getString(R.string.sources_tab) + newLine + getString(R.string.copyright_disclaimer)))
 
-      searchTabPgh1.movementMethod = ScrollingMovementMethod()
-      return view*///super.onCreateView(inflater, container, savedInstanceState)
+        return view
+
     }
 
 
